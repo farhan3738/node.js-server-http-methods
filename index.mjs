776c09047f08e8123a6c157151c075caa2d8fd89
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-20 17:52:51 +0500

    Create a basic server

---
+++ index.mjs
@@ -0,0 +1,15 @@
+import { createServer } from 'node:http';
+
+const PORT = 3000;
+const server = createServer((req, res) => {
+    
+  const url = req.url;
+      if (url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the Home page.');
+        }
+      else if (url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('This is the About page.');
+      };
+    });
