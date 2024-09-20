eb4677add90736a1b20f8cce8f7c294ebd3d9702
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-20 21:04:34 +0500

    Add POST request

--- index.mjs
+++ index.mjs
@@ -26,6 +26,7 @@ const server = createServer((req, res) => {
         res.statusCode = 200;
         res.end(JSON.stringify({ message: 'GET request - Fetching all items' }));
 
+    // POST request
         if (method === 'POST' && parsedUrl.pathname === '/api/items') {
           let body = '';
           req.on('data', chunk => {
@@ -38,28 +39,6 @@ const server = createServer((req, res) => {
             res.end(JSON.stringify({ message: `POST request - Adding new item`, data: newItem }));
           });
         }
-        
-    // PUT Request
-    } 
-    else if (method === 'PUT' && parsedUrl.pathname.startsWith('/api/items/')) {
-        let body = '';
-        const itemId = parsedUrl.pathname.split('/').pop();
-
-        req.on('data', chunk => {
-            body += chunk.toString();
-        });
-        req.on('end', () => {
-            const updatedItem = JSON.parse(body);
-            res.statusCode = 200;
-            res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
-        });
-
-    // DELETE Request
-    } 
-    else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
-        const itemId = parsedUrl.pathname.split('/').pop();
-        res.statusCode = 200;
-        res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
 
     // Handle 404 Not Found
     } 
