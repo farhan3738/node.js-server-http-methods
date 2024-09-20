9dac7871af209d05e4ac00df397f54e19ea049fb
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-20 20:56:30 +0500

    Add GET request

--- index.mjs
+++ index.mjs
@@ -16,6 +16,58 @@ const server = createServer((req, res) => {
       };
     });
 
+    const { method, url } = req;
+    const parsedUrl = new URL(url, `http://${req.headers.host}`);
+
+    // Set response header to JSON & GET request
+      res.setHeader('Content-Type', 'application/json');
+
+      if (method === 'GET' && parsedUrl.pathname === '/api/items') {
+        res.statusCode = 200;
+        res.end(JSON.stringify({ message: 'GET request - Fetching all items' }));
+
+        if (method === 'POST' && parsedUrl.pathname === '/api/items') {
+          let body = '';
+          req.on('data', chunk => {
+            body += chunk.toString();
+          });
+          req.on('end', () => {
+            const newItem = JSON.parse(body);
+            res.statusCode = 201; //created
+            res.setHeader('Content-Type', 'application/json'); // Set Content-Type header
+            res.end(JSON.stringify({ message: `POST request - Adding new item`, data: newItem }));
+          });
+        }
+        
+    // PUT Request
+    } 
+    else if (method === 'PUT' && parsedUrl.pathname.startsWith('/api/items/')) {
+        let body = '';
+        const itemId = parsedUrl.pathname.split('/').pop();
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            const updatedItem = JSON.parse(body);
+            res.statusCode = 200;
+            res.end(JSON.stringify({ message: `PUT request - Updating item ${itemId}`, data: updatedItem }));
+        });
+
+    // DELETE Request
+    } 
+    else if (method === 'DELETE' && parsedUrl.pathname.startsWith('/api/items/')) {
+        const itemId = parsedUrl.pathname.split('/').pop();
+        res.statusCode = 200;
+        res.end(JSON.stringify({ message: `DELETE request - Deleting item ${itemId}` }));
+
+    // Handle 404 Not Found
+    } 
+    else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: 'Route not found' }));
+    }
+
     // Set server on PORT = 3000
     server.listen(PORT, () => {
       console.log(`Server is listening here -> http://localhost:${PORT}`)
