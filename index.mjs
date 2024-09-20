3cdf434e8f3ee2a983db127a3f2f943a6e621fd1
Muhammad Farhan Elahi <mobicare.pew@gmail.com>
2024-09-20 19:09:33 +0500

    Add comments

--- index.mjs
+++ index.mjs
@@ -1,19 +1,22 @@
-import { createServer } from 'node:http';
+import { createServer } from 'node:http'; // Import createServer
 
 const PORT = 3000;
 const server = createServer((req, res) => {
     
-  const url = req.url;
+  const url = req.url; 
+      // Add route '/'
       if (url === '/') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the Home page.');
         }
+        // Add another route '/about'
       else if (url === '/about') {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('This is the About page.');
       };
     });
 
+    // Set server on PORT = 3000
     server.listen(PORT, () => {
       console.log(`Server is listening here -> http://localhost:${PORT}`)
   });
